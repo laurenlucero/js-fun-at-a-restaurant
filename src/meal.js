3a0232fd60d9b2b2179f16ce34114e8d0a5a491e
“Lauren <lucerolaurenm@gmail.com>
2021-05-14 11:26:36 -0400

    Pass meal tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,47 @@
-function nameMenuItem() {
-
+function nameMenuItem(item) {
+  let menuItemName = `Delicious ${item}`;
+  return menuItemName;
 }
 
+function createMenuItem(name, price, type) {
+  let menuItem = {
+    name,
+    price,
+    type,
+  };
+  return menuItem;
+}
+
+function addIngredients(ingredient, ingredients) {
+  if (!ingredients.includes(ingredient)) {
+    ingredients.push(ingredient);
+  }
+}
+
+function formatPrice(initialPrice) {
+  let formattedPrice = `$${initialPrice}`;
+  return formattedPrice;
+}
+
+function decreasePrice(price) {
+  let decreasedPrice = price - 0.1 * price;
+  return decreasedPrice;
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  let recipe = {
+    title,
+    ingredients,
+    type: menuItemType,
+  };
+  return recipe;
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
-}
-
-
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe,
+};
