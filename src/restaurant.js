e2e61dfd531547942bbda6d6b6e91b46784e977a
“Lauren <lucerolaurenm@gmail.com>
2021-05-24 14:27:10 -0400

    Restaurant tests

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,24 @@
+function createRestaurant(name) {
+  return { name: name, menus: { breakfast: [], lunch: [], dinner: [] } };
+}
 
+function addMenuItem(restaurant, item) {
+  if (!restaurant.menus[item.type].includes(item))
+    restaurant.menus[item.type].push(item);
+}
+
+function removeMenuItem(restaurant, name, type) {
+  for (let i = 0; i < restaurant.menus[type].length; i++) {
+    if (restaurant.menus[type][i].name.includes(name)) {
+      restaurant.menus[type].splice(name, 1);
+      return `No one is eating our ${name} - it has been removed from the ${type} menu!`;
+    }
+  }
+  return `Sorry, we don't sell ${name}, try adding a new recipe!`;
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
-}
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem,
+};
